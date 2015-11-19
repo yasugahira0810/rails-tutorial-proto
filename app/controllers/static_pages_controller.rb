e4da9a8b2863d1b7daacedb62bcdcde17054a998
Yuta Yasugahira <hourou_freak@yahoo.co.jp>
2015-11-19 15:47:56 +0000

    Finish layout and routes

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,3 +1,4 @@
+# このファイルの役割ってなんだっけ？
 class StaticPagesController < ApplicationController
   def home
   end
@@ -6,5 +7,8 @@ class StaticPagesController < ApplicationController
   end
   
   def about
+  end
+  
+  def contact
   end
 end
