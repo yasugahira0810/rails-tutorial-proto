30f66ff0e78807fc148eefa13050d4b640d24cbe
Yuta Yasugahira <hourou_freak@yahoo.co.jp>
2015-11-28 14:34:06 +0000

    implement till 11.2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,9 +6,10 @@ class UsersController < ApplicationController
   def index
     @users = User.paginate(page: params[:page])
   end
-
+  
   def show
     @user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(page: params[:page])
   end
 
   def new
