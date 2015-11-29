82b25155c62d9615c2e9efeb5d1818318d04f7e6
Yuta Yasugahira <hourou_freak@yahoo.co.jp>
2015-11-29 00:32:39 +0000

    implement till 11.3.2

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,14 +1,15 @@
-# このファイルの役割ってなんだっけ？
 class StaticPagesController < ApplicationController
+
   def home
+    @micropost = current_user.microposts.build if logged_in?
   end
 
   def help
   end
-  
+
   def about
   end
-  
+
   def contact
   end
 end
