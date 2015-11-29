bc60e5847fa79bec93a500d0fa5d7330a1916414
Yuta Yasugahira <hourou_freak@yahoo.co.jp>
2015-11-29 02:40:14 +0000

    implement till 11.3.3

--- app/models/user.rb
+++ app/models/user.rb
@@ -68,6 +68,12 @@ class User < ActiveRecord::Base
     reset_sent_at < 2.hours.ago
   end
 
+  # 試作feedの定義
+  # 完全な実装は第12章「ユーザーをフォローする」を参照してください。
+  def feed
+    Micropost.where("user_id = ?", id)
+  end
+
 private
 
     # メールアドレスをすべて小文字にする
